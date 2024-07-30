7be308380c11aa3f696c84005f0bf5f8244ceb63
Julian Laubstein <contact@julianlaubstein.de>
2024-07-30 15:40:17 +0200

    Use complex computing function

--- src/DataRenderer.svelte
+++ src/DataRenderer.svelte
@@ -1,9 +1,11 @@
 <script>
+  import { complexComputing } from "./complexComputing.js";
+
   // This is a Svelte 4 component (no runes mode)
 
   export let data;
 
-  $: churnedData = structuredClone(data);
+  $: churnedData = complexComputing(data);
 </script>
 
 <pre>{JSON.stringify(data)}</pre>
